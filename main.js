322f3c551d2224be8d218ad3aa930d479bd7c4c5
David Heasman <1979286+david-heasman00@users.noreply.github.com>
2026-02-18 15:04:21 +0000

    Task 3 submission
    Task 3: "Use an array method to add the words 'to' and 'Program' as separate strings to the end of the secretMessage array."

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 
+//Task 1 and 2
 console.log(secretMessage.length);
 secretMessage.pop();
+console.log(secretMessage.length);
+
+//Task 3
+secretMessage.push('to', 'Program');
 console.log(secretMessage.length);
